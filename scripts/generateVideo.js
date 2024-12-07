a772229d410778da0bac7e447a8cf417ba4a650f
yadavtanmay <tanmayyadav2323@gmail.com>
2024-12-07 09:40:18 +0530

    commit

--- scripts/generateVideo.js
+++ scripts/generateVideo.js
@@ -8,6 +8,7 @@ import fs from "fs/promises";
 import path from "path";
 import { fileURLToPath } from 'url'; 
 import { dirname } from 'path';
+import express from "express"
 
 const __filename = fileURLToPath(import.meta.url); 
 const __dirname = dirname(__filename);
