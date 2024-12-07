98da535f905f244535935af3d12061febeeb8513
yadavtanmay <tanmayyadav2323@gmail.com>
2024-12-07 09:37:24 +0530

    commit

--- scripts/generateVideo.js
+++ scripts/generateVideo.js
@@ -69,6 +69,18 @@ async function main() {
   }
 }
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Service is running!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+
 main();
 
 let isProcessing = false;
